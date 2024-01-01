f48581365011377b55aa8468a8fd511e3ecd25a7
pvdwijdeven <pvdwijdeven@gmail.com>
2024-01-01 16:26:25 +0000

    updated tab

--- Algorithms/General/primes.py
+++ Algorithms/General/primes.py
@@ -27,7 +27,7 @@ def primes(n):
         if sieve[i]:
             sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
     return [2] + [i for i in range(3,n,2) if sieve[i]]
-	
+
 # for big prime numbers:
 def _try_composite(a, d, n, s):
     if pow(a, d, n) == 1:
