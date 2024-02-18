f6ad9fa3ea5ce9ce728ade932d8fad7f4f786939
pvdwijdeven <pvdwijdeven@gmail.com>
2024-02-18 11:17:03 +0100

    linkedlistclass

--- G4G/DSA Course/LinkedList/LinkedList.py
+++ G4G/DSA Course/LinkedList/LinkedList.py
@@ -8,318 +8,304 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self):
-        self.head = Node(None)
-        self.tail = Node(None)
+    def __init__(self, arr=None):
+        if not arr:
+            self.head = Node(None)
+            self.tail = Node(None)
+        else:
+            self.listFromString(arr)
 
-
-def listFromString(slist):
-    llist = [int(x) for x in slist.split("->")]
-    prev = Node(None)
-    head = prev
-    for x in llist:
-        cur = Node(x)
-        prev.next = cur  # type: ignore
-        prev = cur
-    return head.next
-
-
-def printList(head):
-    array = []
-    curr = head
-    while True:
-        array.append(curr.data)
-        if curr.next == None:
-            break
-        curr = curr.next
-    return array
-
-
-def getCount(head):
-    cur = head
-    count = 1
-    while cur.next:
-        count += 1
-        cur = cur.next
-    return count
-
-
-def sumOfElements(head):
-    cur = head
-    total = cur.data
-    while cur.next != None:
-        cur = cur.next
-        total += cur.data
-    return total
-
-
-def searchLinkedList(head, x):
-    cur = head
-    index = 0
-    while cur.next != None:
-        if cur.data == x:
-            return index
-        index += 1
-        cur = cur.next
-    return index
-
-
-def insertAtBegining(head, x):
-    # code here
-    cur = Node(x)
-    cur.next = head
-    return cur
-
-
-# Function to insert a node at the end of the linked list.
-def insertAtEnd(head, x):
-    # code here
-    if head == None:
-        return Node(x)
-    cur = head
-    while cur.next != None:
-        cur = cur.next
-    newNode = Node(x)
-    cur.next = newNode
-    return head
-
-
-def insertAtPosition(head, pos, data):
-    temp = Node(data)
-    if pos == 0:
-        temp.next = head
-        return temp
-    curr = head
-    for i in range(1, pos):
-        curr = curr.next
-        if curr == None:
-            return head
-    temp.next = curr.next
-    curr.next = temp
-    return head
-
-
-def deleteHead(head):
-    # code here
-    if head == None:
-        return head
-    else:
-        temp = head.next
-        head.next = None
-        return temp
-
-
-def deleteTail(head):
-    # code here
-    if head == None or head.next == None:
-        return None
-    cur = head
-    while cur.next.next != None:
-        cur = cur.next
-    cur.next = None
-    return head
-
-
-def deleteNode(ptr):
-    temp = ptr.next
-    ptr.data = temp.data
-    ptr.next = temp.next
-
-
-def deleteAtPosition(head, pos):
-    curr = head
-    c = 1
-    if pos == 1:
+    def listFromString(self, slist):
+        llist = [int(x) for x in slist.split("->")]
+        prev = Node(None)
+        head = prev
+        for x in llist:
+            cur = Node(x)
+            prev.next = cur  # type: ignore
+            prev = cur
+        self.head = head.next
         return head.next
 
-    while curr != None:
-        if c + 1 == pos:
-            temp = curr.next.next
-            curr.next = temp
-        curr = curr.next
-        c += 1
-    return head
-
-
-def insertInSorted(head, data):
-    temp = Node(data)
-
-    if head == None:
-        return temp
-    elif data < head.data:
-        temp.next = head
-        return temp
-    else:
-        curr = head
-
-        while curr.next != None and curr.next.data < data:
+    def printList(self):
+        array = []
+        curr = self.head
+        while True:
+            array.append(curr.data)
+            if curr.next == None:
+                break
             curr = curr.next
+        return array
 
+    def getCount(self):
+        cur = self.head
+        count = 1
+        while cur.next:
+            count += 1
+            cur = cur.next
+        return count
+
+    def sumOfElements(self):
+        cur = self.head
+        total = cur.data
+        while cur.next != None:
+            cur = cur.next
+            total += cur.data
+        return total
+
+    def searchLinkedList(self, x):
+        cur = self.head
+        index = 0
+        while cur.next != None:
+            if cur.data == x:
+                return index
+            index += 1
+            cur = cur.next
+        return index
+
+    def insertAtBegining(self, x):
+        # code here
+        cur = Node(x)
+        cur.next = self.head
+        self.head = cur
+        return cur
+
+    # Function to insert a node at the end of the linked list.
+    def insertAtEnd(self, x):
+        # code here
+        if self.head == None:
+            return Node(x)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        newNode = Node(x)
+        cur.next = newNode
+        return self.head
+
+    def insertAtPosition(self, pos, data):
+        temp = Node(data)
+        if pos == 0:
+            temp.next = self.head
+            return temp
+        curr = self.head
+        for i in range(1, pos):
+            curr = curr.next
+            if curr == None:
+                return self.head
         temp.next = curr.next
         curr.next = temp
-        return head
+        return self.head
 
-
-def insertInMid(head, node):
-    if head == None:
-        return node
-    slow = head
-    fast = head
-    while fast.next != None and fast.next.next != None:
-        slow = slow.next
-        fast = fast.next.next
-    node.next = slow.next
-    slow.next = node
-    return head
-
-
-def isSorted(head):
-    # code here
-    curN = head
-    curD = head.data
-    direction = "equal"
-    while curN.next != None:
-        curN = curN.next
-        if curD > curN.data:
-            if direction == "equal" or direction == "decreasing":
-                direction = "decreasing"
-            else:
-                return False
-        if curD < curN.data:
-            if direction == "equal" or direction == "increasing":
-                direction = "increasing"
-            else:
-                return False
-        curD = curN.data
-    return True
-
-
-def getNthFromLast(head, n):
-    # code here
-    if n == 0:
-        return -1
-
-    if not head:
-        return -1
-
-    cur1 = head
-    cur2 = head
-
-    while cur1:
-        if n:
-            cur1 = cur1.next
-            n -= 1
+    def deleteHead(self):
+        # code here
+        if self.head == None:
+            return self.head
         else:
-            cur1 = cur1.next
-            cur2 = cur2.next
-    if n:
-        return -1
+            temp = self.head.next
+            self.head.next = None
+            self.head = temp
+            return temp
 
-    return cur2.data
-
-
-def joinTheLists(head1, head2):
-    # code here
-    if head1 == None:
-        return head2
-    cur = head1
-    while cur.next != None:
-        cur = cur.next
-    cur.next = head2
-    return head1
-
-
-def removeDuplicates(head):
-    # code here
-    if head == None:
-        return None
-    cur = head
-    while cur.next != None:
-        if cur.data == cur.next.data:
-            cur.next = cur.next.next
-        else:
+    def deleteTail(self):
+        # code here
+        if not self.head or not self.head.next:
+            return None
+        cur = self.head
+        while cur.next.next != None:
             cur = cur.next
-    return head
+        cur.next = None
+        return self.head
 
+    def deleteNode(self, ptr):
+        temp = ptr.next
+        ptr.data = temp.data
+        ptr.next = temp.next
 
-def reverseList(head):
-    # Code here
-    stack = []
-    if head == None:
-        return None
-    cur = head
-    while cur.next != None:
-        stack.append(cur)
-        cur = cur.next
-    head = cur
-    while stack != []:
-        cur.next = stack.pop()
-        cur = cur.next
-    cur.next = None
-    return head
+    def deleteAtPosition(self, pos):
+        curr = self.head
+        c = 1
+        if pos == 1:
+            return self.head.next
 
+        while curr:
+            if c + 1 == pos:
+                temp = curr.next.next
+                curr.next = temp
+            curr = curr.next
+            c += 1
+        return self.head
 
-def maximum(head):
-    # code here
-    mx = 0
-    cur = head
-    while cur.next != None:
+    def insertInSorted(self, data):
+        temp = Node(data)
+
+        if not self.head:
+            return temp
+        elif data < self.head.data:
+            temp.next = self.head
+            self.head = temp
+            return temp
+        else:
+            curr = self.head
+
+            while curr.next != None and curr.next.data < data:
+                curr = curr.next
+
+            temp.next = curr.next
+            curr.next = temp
+            return self.head
+
+    def insertInMid(self, node):
+        if self.head == None:
+            return node
+        slow = self.head
+        fast = self.head
+        while fast.next != None and fast.next.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        node.next = slow.next
+        slow.next = node
+        return self.head
+
+    def isSorted(self):
+        # code here
+        curN = self.head
+        curD = self.head.data
+        direction = "equal"
+        while curN.next != None:
+            curN = curN.next
+            if curD > curN.data:
+                if direction == "equal" or direction == "decreasing":
+                    direction = "decreasing"
+                else:
+                    return False
+            if curD < curN.data:
+                if direction == "equal" or direction == "increasing":
+                    direction = "increasing"
+                else:
+                    return False
+            curD = curN.data
+        return True
+
+    def getNthFromLast(self, n):
+        # code here
+        if n == 0:
+            return -1
+
+        if not self.head:
+            return -1
+
+        cur1 = self.head
+        cur2 = self.head
+
+        while cur1:
+            if n:
+                cur1 = cur1.next
+                n -= 1
+            else:
+                cur1 = cur1.next
+                cur2 = cur2.next
+        if n:
+            return -1
+
+        return cur2.data
+
+    def joinTheLists(self, head2):
+        # code here
+        if self.head == None:
+            return head2
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = head2
+        return self.head
+
+    def removeDuplicates(self):
+        # code here
+        if not self.head:
+            return None
+        cur = self.head
+        while cur.next != None:
+            if cur.data == cur.next.data:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return self.head
+
+    def reverseList(self):
+        # Code here
+        stack = []
+        if not self.head:
+            return None
+        cur = self.head
+        while cur.next != None:
+            stack.append(cur)
+            cur = cur.next
+        head = cur
+        while stack != []:
+            cur.next = stack.pop()
+            cur = cur.next
+        cur.next = None
+        return self.head
+
+    def maximum(self):
+        # code here
+        mx = 0
+        cur = self.head
+        while cur.next != None:
+            if cur.data > mx:
+                mx = cur.data
+            cur = cur.next
         if cur.data > mx:
             mx = cur.data
-        cur = cur.next
-    if cur.data > mx:
-        mx = cur.data
-    return mx
+        return mx
 
-
-def minimum(head):
-    # code here
-    mn = 10**199
-    cur = head
-    while cur.next != None:
+    def minimum(self):
+        # code here
+        mn = 10**199
+        cur = self.head
+        while cur.next != None:
+            if cur.data < mn:
+                mn = cur.data
+            cur = cur.next
         if cur.data < mn:
             mn = cur.data
-        cur = cur.next
-    if cur.data < mn:
-        mn = cur.data
-    return mn
+        return mn
 
-
-def areIdentical(head1, head2):
-    # Code here
-    cur1 = head1
-    cur2 = head2
-    while cur1.next != None and cur2.next != None:
+    def areIdentical(self, head2):
+        # Code here
+        cur1 = self.head
+        cur2 = head2
+        while cur1.next != None and cur2.next != None:
+            if cur1.data != cur2.data:
+                return False
+            cur1 = cur1.next
+            cur2 = cur2.next
+        if cur1.next != cur2.next:
+            return False
         if cur1.data != cur2.data:
             return False
-        cur1 = cur1.next
-        cur2 = cur2.next
-    if cur1.next != cur2.next:
-        return False
-    if cur1.data != cur2.data:
-        return False
-    return True
+        return True
 
 
 l1 = "1->2->3->4->5"
-l2 = "2->4->6->7->5->1->0"
 
-L1 = listFromString(l1)
-print(printList(L1))
-print(getCount(L1))
-print(sumOfElements(L1))
-print(searchLinkedList(L1, 3))
 
-L1 = insertAtBegining(L1, 9)
-L1 = insertAtEnd(L1, 8)
+L1 = LinkedList()
+L1.listFromString(l1)
+print(L1.printList())
+print(L1.getCount())
+print(L1.sumOfElements())
+print(L1.searchLinkedList(3))
 
-L1 = insertAtPosition(L1, 7, 3)
+L1.insertAtBegining(9)
+L1.insertAtEnd(8)
 
-L1 = deleteHead(L1)
+L1.insertAtPosition(7, 3)
 
-print(printList(L1))
+L1.deleteHead()
 
-L2 = listFromString("1->1->1->3->3->3->4->4")
+print(L1.printList())
+l2 = "1->1->1->3->3->3->4->4"
+L2 = LinkedList(l2)
 # L2.removeDuplicates(L2.head)
-L2 = reverseList(L2)
-print(printList(L2))
+L2.reverseList()
+print(L2.printList())
