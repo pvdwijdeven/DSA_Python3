c6c05a9e3448b019db4f08d4f1d89b32149654c9
pvdwijdeven <pvdwijdeven@gmail.com>
2024-02-23 19:26:40 +0100

    pop8

--- G4G/DSA Course/BinaryTrees/BinaryTreeClass.py
+++ G4G/DSA Course/BinaryTrees/BinaryTreeClass.py
@@ -1,48 +1,51 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, val):
+    def __init__(self, val) -> None:
         self.data = val
         self.left = None
         self.right = None
 
 
 class BT:
-    def __init__(self, arr=None):
+    def __init__(self, arr=None) -> None:
         # create empty root
         if not arr:
-            self.root = Node(None)
+            self.root = Node(val=None)
         else:
-            self.build_tree(arr)
+            self.build_tree(lst=arr)
 
-    def get_root(self):
+    def get_root(self) -> Node | None:
         # function to retrieve root for external programs
         return self.root
 
-    def build_tree(self, lst):
+    def build_tree(self, lst) -> Node | None:
         # funtion to build tree from an array, where "N" represents empty node
-        def grab(it, next_level):
+        def grab(it, next_level) -> Node | None:
             value = next(it, "N")
             if value != "N":
-                node = Node(value)
+                node = Node(val=value)
                 next_level.append(node)
                 return node
 
         # Create the root of the tree
         it = iter(lst)
         next_level = []
-        self.root = grab(it, next_level)
+        self.root = grab(it=it, next_level=next_level)
 
         while next_level:
             level = next_level
             next_level = []
             for node in level:
-                node.left = grab(it, next_level)
-                node.right = grab(it, next_level)
+                node.left = grab(it=it, next_level=next_level)
+                node.right = grab(it=it, next_level=next_level)
 
         return self.root
 
-    def print_tree(self, val="data", left="left", right="right"):
+    def print_tree(self, val="data", left="left", right="right") -> None:
         # function to print tree in a grahical way
-        def display(root, val=val, left=left, right=right):
+        def display(root, val=val, left=left, right=right) -> Any:
             # Returns list of strings, width, height, and horizontal coordinate of the root.
             # No child.
             if getattr(root, right) is None and getattr(root, left) is None:
@@ -69,7 +72,7 @@ class BT:
 
             # Only right child.
             if getattr(root, left) is None:
-                lines, n, p, x = display(getattr(root, right))
+                lines, n, p, x = display(root=getattr(root, right))
                 s = "%s" % getattr(root, val)
                 u = len(s)
                 first_line = s + x * "_" + (n - x) * " "
@@ -78,8 +81,8 @@ class BT:
                 return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
 
             # Two children.
-            left, n, p, x = display(getattr(root, left))
-            right, m, q, y = display(getattr(root, right))
+            left, n, p, x = display(root=getattr(root, left))
+            right, m, q, y = display(root=getattr(root, right))
             s = "%s" % getattr(root, val)
             u = len(s)
             first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
@@ -96,29 +99,29 @@ class BT:
             ]
             return lines, n + m + u, max(p, q) + 2, n + u // 2
 
-        lines, *_ = display(self.root, val, left, right)
+        lines, *_ = display(root=self.root, val=val, left=left, right=right)
         for line in lines:
             print(line)
 
-    def is_balanced(self):
+    def is_balanced(self) -> bool:
         # function to check if tree is balanced
-        def height(root):
+        def height(root) -> int:
             if not root:
                 return 0
-            leftheight, rightheight = height(root.left), height(root.right)
+            leftheight, rightheight = height(root=root.left), height(root=root.right)
             if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:
                 return -1
             return max(leftheight, rightheight) + 1
 
-        return height(self.root) >= 0
+        return height(root=self.root) >= 0
 
-    def is_sum_property(self):
+    def is_sum_property(self) -> bool:
         # check if parent is always sum of children
-        def scan_tree(root):
+        def scan_tree(root) -> bool:
             res = True
             if root:
-                res = res and scan_tree(root.left)
-                res = res and scan_tree(root.right)
+                res = res and scan_tree(root=root.left)
+                res = res and scan_tree(root=root.right)
                 lval = 0 if not root.left else root.left.data
                 rval = 0 if not root.right else root.right.data
                 return res and (
@@ -126,54 +129,58 @@ class BT:
                 )
             return res
 
-        return scan_tree(self.root)
+        return scan_tree(root=self.root)
 
-    def get_height(self):
+    def get_height(self) -> int:
         # return height of tree
-        def height(root):
-            return 0 if not root else max(height(root.left) + 1, height(root.right) + 1)
+        def height(root) -> int:
+            return (
+                0
+                if not root
+                else max(height(root=root.left) + 1, height(root=root.right) + 1)
+            )
 
-        return height(self.root)
+        return height(root=self.root)
 
-    def get_pre_order(self):
+    def get_pre_order(self) -> list[Any]:
         # get pre-order traversal array of tree
-        def pre_order(root):
+        def pre_order(root) -> list[Any]:
             arr = []
             if root:
                 arr.append(root.data)
-                arr += pre_order(root.left)
-                arr += pre_order(root.right)
+                arr += pre_order(root=root.left)
+                arr += pre_order(root=root.right)
             return arr
 
-        return pre_order(self.root)
+        return pre_order(root=self.root)
 
-    def get_in_order(self):
+    def get_in_order(self) -> list[Any]:
         # get in-order traversal array of tree
-        def in_order(root):
+        def in_order(root) -> list[Any]:
             arr = []
             if root:
-                arr += in_order(root.left)
+                arr += in_order(root=root.left)
                 arr.append(root.data)
-                arr += in_order(root.right)
+                arr += in_order(root=root.right)
             return arr
 
-        return in_order(self.root)
+        return in_order(root=self.root)
 
-    def get_post_order(self):
+    def get_post_order(self) -> list[Any]:
         # get post-order traversal array of tree
-        def post_order(root):
+        def post_order(root) -> list[Any]:
             arr = []
             if root:
-                arr += post_order(root.left)
-                arr += post_order(root.right)
+                arr += post_order(root=root.left)
+                arr += post_order(root=root.right)
                 arr.append(root.data)
             return arr
 
-        return post_order(self.root)
+        return post_order(root=self.root)
 
-    def get_level_order(self):
+    def get_level_order(self) -> list[Any]:
         # get level-order traversal array of tree
-        def level_order(root):
+        def level_order(root) -> list[Any]:
             if not root:
                 return []
             arr = []
@@ -188,29 +195,29 @@ class BT:
                     queue.append(node.right)
             return arr
 
-        return level_order(self.root)
+        return level_order(root=self.root)
 
     @classmethod
-    def compare_tree(cls, root1, root2):
-        def is_identical(root1, root2):
-            # code here
+    def compare_tree(cls, root1, root2) -> bool:
+        def is_identical(root1, root2) -> bool:
+
             res = True
 
             if root1 and root2:
                 if root1.data != root2.data:
                     return False
-                res = is_identical(root1.left, root2.left)
-                res = res and is_identical(root1.right, root2.right)
+                res = is_identical(root1=root1.left, root2=root2.left)
+                res = res and is_identical(root1=root1.right, root2=root2.right)
             else:
                 if root1 != root2:
                     return False
             return res
 
-        return is_identical(root1.root, root2.root)
+        return is_identical(root1=root1.root, root2=root2.root)
 
 
 if __name__ == "__main__":
-    bt = BT([3, 2, 1, 4, 5])
+    bt = BT(arr=[3, 2, 1, 4, 5])
     bt.print_tree()
     print(bt.is_balanced())
     print(bt.is_sum_property())
@@ -220,8 +227,8 @@ if __name__ == "__main__":
     print(bt.get_post_order())
     print(bt.get_level_order())
 
-    bt2 = BT([3, 2, 1, 4, 5])
-    bt3 = BT([3, "N", 1, 4, 5])
+    bt2 = BT(arr=[3, 2, 1, 4, 5])
+    bt3 = BT(arr=[3, "N", 1, 4, 5])
     bt3.print_tree()
-    print(BT.compare_tree(bt, bt2))
-    print(BT.compare_tree(bt, bt3))
+    print(BT.compare_tree(root1=bt, root2=bt2))
+    print(BT.compare_tree(root1=bt, root2=bt3))
--- G4G/DSA Course/LinkedList/DoubleLinkedListClass.py
+++ G4G/DSA Course/LinkedList/DoubleLinkedListClass.py
@@ -31,26 +31,26 @@ class Node:
 class DoubleLinkedList:
     def __init__(self, arr=None):
         if not arr:
-            self.head = Node(None)
+            self.head = Node(data=None)
         else:
             if isinstance(arr, list):
-                self.list_from_arr(arr)
+                self.list_from_arr(arr=arr)
             else:
-                self.list_from_string(arr)
+                self.list_from_string(slist=arr)
 
     def list_from_string(self, slist):
         llist = [int(x) for x in slist.split("<->")]
-        return self.list_from_arr(llist)
+        return self.list_from_arr(arr=llist)
 
     def list_from_arr(self, arr):
         if arr == []:
-            self.head = Node(None)
+            self.head = Node(data=None)
             return self.head
-        prev_node = Node(None)
+        prev_node = Node(data=None)
         prev_prev = None
         head = prev_node
-        for x in arr:
-            cur = Node(x)
+        for value in arr:
+            cur = Node(data=value)
             prev_node.next = cur
             prev_node.prev = prev_prev
             prev_prev = prev_node
@@ -62,7 +62,6 @@ class DoubleLinkedList:
         return head.next
 
     def print_list(self):
-        # code here
         res = []
         cur = self.head
         if not cur:
@@ -74,8 +73,7 @@ class DoubleLinkedList:
         return res
 
     def insert_in_head(self, data):
-        # code here
-        temp = Node(data)
+        temp = Node(data=data)
         temp.prev = None
         temp.next = self.head
         if self.head:
@@ -84,8 +82,7 @@ class DoubleLinkedList:
         return temp
 
     def insert_in_tail(self, data):
-        # code here
-        temp = Node(data)
+        temp = Node(data=data)
         if not self.head:
             return temp
         cur = self.head
@@ -95,10 +92,10 @@ class DoubleLinkedList:
         temp.prev = cur
         return self.head
 
-    def insert_at_position(self, p, data):
-        temp = Node(data)
+    def insert_at_position(self, pos, data):
+        temp = Node(data=data)
         cur = self.head
-        for i in range(0, p):
+        for i in range(0, pos):
             if cur:
                 cur = cur.next
             if not cur:
@@ -121,10 +118,9 @@ class DoubleLinkedList:
             self.head.prev = None
             return self.head
 
-    def delete_node(self, x):
-        # Code here
+    def delete_Nth_node(self, N):
         cur = self.head
-        for pos in range(1, x):
+        for pos in range(1, N):
             if cur:
                 cur = cur.next
         if cur:
@@ -135,13 +131,12 @@ class DoubleLinkedList:
         return self.head
 
     def delete_tail(self):
-        # code here
         if not self.head:
             return None
         if not self.head.next:
             return None
         cur = self.head
-        prev = Node(None)
+        prev = Node(data=None)
         while cur.next:
             prev = cur
             cur = cur.next
@@ -149,7 +144,6 @@ class DoubleLinkedList:
         return self.head
 
     def find_middle(self):
-        # code here
         if self.head:
             curl = self.head
             cur = self.head.prev
@@ -181,19 +175,18 @@ class DoubleLinkedList:
         self.head = cur
         return cur
 
-    def sorted_insert(self, x):
-        # code here
+    def sorted_insert(self, value):
         cur = self.head
         if not cur:
             return None
-        if cur.data >= x:
-            temp = Node(x)
+        if cur.data >= value:
+            temp = Node(data=value)
             cur.prev = temp
             temp.next = cur
             return temp
         while cur.next:
-            if cur.data >= x:
-                temp = Node(x)
+            if cur.data >= value:
+                temp = Node(data=value)
                 if cur.prev:
                     cur.prev.next = temp
                 temp.prev = cur.prev
@@ -202,29 +195,29 @@ class DoubleLinkedList:
                 return self.head
             else:
                 cur = cur.next
-        temp = Node(x)
+        temp = Node(data=value)
         cur.next = temp
         temp.prev = cur
         return self.head
 
 
 if __name__ == "__main__":
-    L1 = DoubleLinkedList("1<->2<->3<->4<->5")
+    L1 = DoubleLinkedList(arr="1<->2<->3<->4<->5")
     L1.print_all()
     print(L1.print_list())
 
-    L1.insert_in_head(20)
-    L1.insert_in_tail(44)
-    L1.insert_at_position(2, 50)
+    L1.insert_in_head(data=20)
+    L1.insert_in_tail(data=44)
+    L1.insert_at_position(pos=2, data=50)
     print(L1.print_list())
 
-    L2 = DoubleLinkedList("2<->4<->5")
-    L2.insert_at_position(2, 6)
+    L2 = DoubleLinkedList(arr="2<->4<->5")
+    L2.insert_at_position(pos=2, data=6)
     print(L2.print_list())
     L2.print_all()
     L2.reverse_list()
     print(L2.print_list())
 
-    L3 = DoubleLinkedList([1, 9, 16, 25, 78])
-    L3.sorted_insert(0)
+    L3 = DoubleLinkedList(arr=[1, 9, 16, 25, 78])
+    L3.sorted_insert(value=0)
     print(L3.print_list())
