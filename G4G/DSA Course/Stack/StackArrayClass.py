c0f479458cc8b603269bf834db7bc0c10a119848
pvdwijdeven <pvdwijdeven@gmail.com>
2024-02-24 21:30:44 +0100

    stack update

--- G4G/DSA Course/Stack/StackArrayClass.py
+++ G4G/DSA Course/Stack/StackArrayClass.py
@@ -1,5 +1,8 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, value):
+    def __init__(self, value) -> None:
         self.value = value
         self.next = None
 
@@ -8,23 +11,23 @@ class Stack:
     # Initializing a stack.
     # Use a dummy node, which is
     # easier for handling edge cases.
-    def __init__(self):
+    def __init__(self) -> None:
         self.stack = []
 
     # String representation of the stack
-    def __str__(self):
+    def __str__(self) -> str:
         return "".join([str(x) for x in self.stack])
 
     # Get the current size of the stack
-    def getSize(self):
+    def getSize(self) -> int:
         return len(self.stack)
 
     # Check if the stack is empty
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return self.stack == []
 
     # Get the top item of the stack
-    def top(self):
+    def top(self) -> Any:
         # Sanitary check to see if we
         # are peeking an empty stack.
         if self.isEmpty():
@@ -32,11 +35,11 @@ class Stack:
         return self.stack[-1]
 
     # Push a value into the stack.
-    def push(self, value):
+    def push(self, value) -> None:
         self.stack.append(value)
 
     # Remove a value from the stack and return.
-    def pop(self):
+    def pop(self) -> Any:
         if self.isEmpty():
             raise Exception("Popping from an empty stack")
         return self.stack.pop()
--- G4G/DSA Course/Stack/StackLinedListClass.py
+++ G4G/DSA Course/Stack/StackLinedListClass.py
@@ -1,54 +1,66 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+    def __init__(self, data, next=None) -> None:
+        self.data = data
+        self.next = next
+
+    @property
+    def var_next(self) -> Any:
+        assert self.next is not None
+        return self.next
 
 
 class Stack:
     # Initializing a stack.
     # Use a dummy node, which is
     # easier for handling edge cases.
-    def __init__(self):
-        self.head = Node("head")
+    def __init__(self) -> None:
+        self.head = Node(data="head")
         self.size = 0
 
     # String representation of the stack
-    def __str__(self):
+    def __str__(self) -> str:
         cur = self.head.next
         out = ""
         while cur:
-            out += str(cur.value) + "->"
+            out += str(cur.data) + "->"
             cur = cur.next
         return out[:-3]
 
     # Get the current size of the stack
-    def getSize(self):
+    def getSize(self) -> int:
         return self.size
 
     # Check if the stack is empty
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return self.size == 0
 
     # Get the top item of the stack
-    def top(self):
+    def top(self) -> Any:
         # Sanitary check to see if we
         # are peeking an empty stack.
         if self.isEmpty():
             raise Exception("Peeking from an empty stack")
-        return self.head.next.value  # type: ignore
+        assert self.head.next
+        return self.head.next.data
 
     # Push a value into the stack.
-    def push(self, value):
-        node = Node(value)
-        node.next = self.head.next
-        self.head.next = node  # type: ignore
+    def push(self, value) -> None:
+        temp = Node(data=value)
+        temp.next = self.head.next
+        assert self.head != None
+        self.head.next = temp
         self.size += 1
 
     # Remove a value from the stack and return.
-    def pop(self):
+    def pop(self) -> Any:
         if self.isEmpty():
             raise Exception("Popping from an empty stack")
         remove = self.head.next
-        self.head.next = self.head.next.next  # type: ignore
+        assert self.head.next
+        self.head.next = self.head.next.next
         self.size -= 1
-        return remove.value  # type: ignore
+        assert remove
+        return remove.data
