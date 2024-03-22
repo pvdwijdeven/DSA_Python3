d5e6d72d153470b06daddb10d44f2cfdfc1acef5
pvdwijdeven <pvdwijdeven@gmail.com>
2024-03-22 14:47:47 +0000

    Updated/Optimized quicksort

--- G4G/DSA Course/Sorting/QuickSort.py
+++ G4G/DSA Course/Sorting/QuickSort.py
@@ -1,39 +1,48 @@
-def lomuto_partition(arr, l, h):
-	pivot = arr[h]
-	i = l - 1
-	for j in range(l, h):
+def lomuto_partition(arr, low, high):
+	pivot = arr[high]
+	i = low - 1
+	for j in range(low, high):
 		if arr[j] <= pivot:  # error
 			i = i + 1
 			arr[i], arr[j] = arr[j], arr[i]
 
-	arr[i + 1], arr[h] = arr[h], arr[i + 1]
+	arr[i + 1], arr[high] = arr[high], arr[i + 1]
 	return i + 1
 
 
-def hoarse_partition(arr, l, h):
-	pivot = arr[l]
-	i = l - 1
-	j = h + 1
+def hoarse_partition(arr, low, high):
+	pivot = arr[(high + low) // 2]
+	i = low
+	j = high
+
 	while True:
-		i = i + 1
 		while arr[i] < pivot:
-			i = i + 1
-		j = j - 1
+			i += 1
 		while arr[j] > pivot:
-			j = j - 1
+			j -= 1
 		if i >= j:
 			return j
 		arr[i], arr[j] = arr[j], arr[i]
 
 
-def qSort(arr, l=0, h=None, partition=lomuto_partition):
-	if h == None:
-		h = len(arr) - 1
+def qSort(arr, low=0, high=None, partition=lomuto_partition):
+	if high is None:
+		high = len(arr) - 1
 	par = 0 if partition == hoarse_partition else 1
-	if l < h:
-		p = partition(arr, l, h)
-		qSort(arr=arr, l=l, h=p - par, partition=partition)
-		qSort(arr=arr, l=p + 1, h=h, partition=partition)
+	if low < high:
+		p = partition(arr, low, high)
+		qSort(arr=arr, low=low, high=p - par, partition=partition)
+		qSort(arr=arr, low=p + 1, high=high, partition=partition)
+
+
+def qSort_optimized(arr, low=0, high=None, partition=lomuto_partition):
+	if high is None:
+		high = len(arr) - 1
+	par = 0 if partition == hoarse_partition else 1
+	while low < high:
+		p = partition(arr, low, high)
+		qSort_optimized(arr=arr, low=low, high=p - par, partition=partition)
+		low = p + 1
 
 
 if __name__ == "__main__":
@@ -42,4 +51,10 @@ if __name__ == "__main__":
 	print(*arr)
 	arr = [8, 4, 7, 9, 3, 10, 5]
 	qSort(arr=arr, partition=lomuto_partition)
+	print(*arr)
+	arr = [8, 4, 7, 9, 3, 10, 5]
+	qSort_optimized(arr=arr, partition=hoarse_partition)
+	print(*arr)
+	arr = [8, 4, 7, 9, 3, 10, 5]
+	qSort_optimized(arr=arr, partition=lomuto_partition)
 	print(*arr)
