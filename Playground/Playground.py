0f59009b44d7234c3d1d27e0af18f44a423bcf00
pvdwijdeven <pvdwijdeven@gmail.com>
2024-04-06 22:16:57 +0200

    pattern search final

--- Playground/Playground.py
+++ Playground/Playground.py
@@ -1,14 +1,12 @@
-from collections import defaultdict
-
-
 class Solution:
-    # Function to check if two arrays are equal or not.
-    def check(self, A, B, N):
-        mp = defaultdict(int)
-        for i in range(len(A)):
-            mp[A[i]] += 1
-            mp[B[i]] -= 1
-        for x in mp:
-            if mp[x] != 0:
-                return False
-        return True
+
+    # Function to check if two strings are rotations of each other or not.
+    def areRotations(self, s1, s2):
+        # code here
+        n = len(s1)
+        s_check = s1
+        for i in range(n):
+            if s_check == s2:
+                return True
+            s_check = s_check[1:] + s1[i]
+        return False
