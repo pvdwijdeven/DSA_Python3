7f6dfeeb77f36c1028246a55e2d1a6b5b7237201
Pascal van de Wijdeven <pvdwijdeven@gmail.com>
2024-05-30 21:24:24 +0200

    Uddate graph

---
+++ G4G/DSA Course/Graph/DFS.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+
+class Graph:
+    # Constructor
+    def __init__(self):
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # Function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # A function used by DFS
+
+    def DFSUtil(self, v, visited):
+        # Mark the current node as visited and print it
+        visited.add(v)
+        print(v, end=" ")
+
+        # recur for all the vertices adjacent to this vertex
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+        # The function to do DFS traversal. It uses recursive DFSUtil
+
+    def DFS(self):
+        # create a set to store all visited vertices
+        visited = set()
+        # call the recursive helper function to print DFS traversal starting from all
+        # vertices one by one
+        for vertex in self.graph:
+            if vertex not in visited:
+                self.DFSUtil(vertex, visited)
+
+
+if __name__ == "__main__":
+    g = Graph()
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 3)
+    g.addEdge(4, 4)
+
+    # Function call
+    g.DFS()
